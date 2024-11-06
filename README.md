253512fd7441e03a7955e38fac378b49dcf12a22
sultanbee786 <sulthanbeeshaik31@gmail.com>
2024-11-06 16:31:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Homepage Lab
+
+
+## Description
+
+In this lab you'll learn a handful of common html tags and how to use them.
+
+
+## Where to start
+
+Start by opening the two lab images; these two images describe how to view the HTML pages in this series of labs. After viewing the images, try to view the "Intro.html" file.
+
+You're now ready to begin this series of labs. Begin by opening and viewing the "YourHomepage.html" file; the file itself contains this lab's instructions.
+
+Files to edit:
+- YourHomepage.html
+
+
+
---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,84 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+
+public class SeleniumTest {
+
+    private WebDriver webDriver;
+
+    @Before
+    public void setUp() {
+        // Set up ChromeDriver path
+        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
+
+        // Get file
+        File file = new File("src/main/YourHomePage.html");
+        String path = "file://" + file.getAbsolutePath();
+
+        // Create a new ChromeDriver instance
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        webDriver = new ChromeDriver(options);
+
+        // Open the HTML file
+        webDriver.get(path);
+    }
+
+    @After
+    public void teardown() {
+        webDriver.quit();
+    }
+
+    /*
+    TODO: Write tests
+     */
+
+     @Test
+     public void titleIsChanged() {
+        String title = webDriver.getTitle();
+
+        assertNotEquals("placeholder", title);
+     }
+
+     @Test
+     public void headerTagTest() {
+        boolean isHeaderPresent = false;
+        String[] headerTagNames = {"h1", "h2", "h3", "h4", "h5", "h6"};
+        for(String headerTagName: headerTagNames) {
+            isHeaderPresent = webDriver.findElements(By.tagName(headerTagName)).size() > 0;
+            if (isHeaderPresent) break;
+        }
+        assertTrue(isHeaderPresent);
+
+     }
+
+     @Test
+     public void listTest() {
+        WebElement element = webDriver.findElement(By.cssSelector("ul > li"));
+        assertNotNull(element);
+     }
+
+     @Test
+     public void linkTest() {
+        WebElement element = webDriver.findElement(By.tagName("a"));
+        assertNotNull(element);
+        assertEquals("Intro.html", element.getDomAttribute("href"));
+     }
+
+}
